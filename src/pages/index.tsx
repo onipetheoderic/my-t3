1c820fd6d78381a61f4ebc6c63a1e13030fced8f
theoderic <onipetheoderic@gmail.com>
2023-03-27 21:21:16 +0200

    add sizing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,7 +45,7 @@ const PostView = (props: PostWithUser) => {
           <span>{`@${author.username} `}</span>
           <span className="font-thin">{` . ${dayjs(post.createdAt).fromNow()}`}</span>
         </div>
-        <span>{post.content}</span>
+        <span className="text-2xl">{post.content}</span>
       </div>
 
     </div>
