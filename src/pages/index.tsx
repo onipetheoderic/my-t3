b3f790b6357e7632c6a0a3d09da737fe96a32cad
theoderic <onipetheoderic@gmail.com>
2023-03-31 19:41:50 +0200

    posting, loading, state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import { SignInButton, useUser } from "@clerk/nextjs";
+import toast from 'react-hot-toast';
 
 import { api } from "~/utils/api";
 import { type RouterOutputs } from '../utils/api';
@@ -8,7 +9,7 @@ import { type RouterOutputs } from '../utils/api';
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime"
 import Image from "next/image";
-import { LoadingPage } from "~/components/loading";
+import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
 
 dayjs.extend(relativeTime)
@@ -27,6 +28,16 @@ const CreatePostWizard = () => {
     onSuccess: () => {
       setInput("");
       void ctx.posts.getAll.invalidate()
+    },
+    onError: (e) => {
+      const errorMessage = e.data?.zodError?.fieldErrors.content;
+      if (errorMessage && errorMessage[0]) {
+        toast.error(errorMessage[0])
+      }
+      else {
+        toast.error("Failed to post! Please try again later")
+      }
+
     }
   })
   console.log(user, "the user")
@@ -44,9 +55,27 @@ const CreatePostWizard = () => {
         type="text"
         value={input}
         onChange={(e) => setInput(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            e.preventDefault()
+            if (input !== "") {
+              mutate({ content: input });
+            }
+          }
+        }}
         disabled={isPosting}
       />
-      <button onClick={() => mutate({ content: input })}>Post</button>
+      {input !== "" && !isPosting
+        && (
+          <button onClick={() => mutate({ content: input })}>
+            Post
+          </button>
+        )}
+      {isPosting &&
+        <div className="flex items-center justify-center">
+          <LoadingSpinner size={20} />
+        </div>
+      }
     </div>
   )
 }
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -59,7 +59,8 @@ export const postsRouter = createTRPCRouter({
     //https://zod.dev/
     create: privateProcedure.input(
         z.object({
-            content: z.string().emoji().min(1).max(280)
+            content: z.string().emoji("Only emojis are allowed").min(1).max(280)
+            //content: z.string().emoji().min(1).max(280)
         })).mutation(async ({ ctx, input }) => {
             const authorId = ctx.userId || "";
             const { success } = await ratelimit.limit(authorId)
